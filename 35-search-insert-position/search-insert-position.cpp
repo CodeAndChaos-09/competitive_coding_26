fd772c983b75010b1f00e282b944889c2e9ff1cc
CodeAndChaos-09 <24BAI70594@cuchd.in>
2026-07-14 14:12:18 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (78.10%) - LeetSync

--- 35-search-insert-position/search-insert-position.cpp
+++ 35-search-insert-position/search-insert-position.cpp
@@ -4,25 +4,21 @@ public:
         int n = nums.size();
         int start = 0;
         int end = n - 1;
-        int mid = 0;
 
         while (start <= end) {
-
-            mid = start + (end - start) / 2;
+            int mid = start + (end - start) / 2;
 
             if (nums[mid] == target) {
                 return mid;
             }
-
             else if (nums[mid] < target) {
                 start = mid + 1;
             }
-
             else {
                 end = mid - 1;
             }
         }
 
-        return (target > nums[mid]) ? mid + 1 : mid;
+        return start;
     }
 };
